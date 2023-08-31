006b29e35d92f8adc538d0706273542e828e82c0
Nader <nnabil@youxel.com>
2023-08-31 03:06:26 +0300

    - modify details view visibility

--- movies_listing/src/main/java/com/example/movies_listing/ui/fragment/details/MovieDetailsUiHelper.kt
+++ movies_listing/src/main/java/com/example/movies_listing/ui/fragment/details/MovieDetailsUiHelper.kt
@@ -1,16 +1,15 @@
 package com.example.movies_listing.ui.fragment.details
 
-import android.content.Context
+import androidx.core.view.isVisible
 import com.example.movies_listing.R
 import com.example.movies_listing.databinding.FragmentMovieDetailsBinding
 import com.example.movies_listing.domain.entities.local.MovieDetails
 import com.youxel.core.base.dialog.BaseNetworkingDialog
 import com.youxel.core.base.fragment.BaseUiHelper
-import com.youxel.core.domain.entities.base.ErrorStatus
 import com.youxel.core.utils.loadImg
 import javax.inject.Inject
 
-class MovieDetailsUiHelper @Inject constructor(private val baseNetworkingDialog: BaseNetworkingDialog) :
+class MovieDetailsUiHelper @Inject constructor() :
     BaseUiHelper() {
     fun setData(
         binding: FragmentMovieDetailsBinding,
@@ -19,6 +18,7 @@ class MovieDetailsUiHelper @Inject constructor(private val baseNetworkingDialog:
     ) {
         binding.run {
             movieDetails.run {
+                binding.scrollContent.isVisible = true
                 ivPoster.loadImg(posterImage)
                 ivBackdrop.loadImg(backdropImage)
                 tvMovieTitle.text = title
